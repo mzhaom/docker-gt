45d18fcaa17d199083301c7007ddcbde4a876d1d
mzhao <mzhao@test8.lab.luminatewireless.com>
2014-11-26 16:18:56 -0800

    Add more config.

--- start-builder.sh
+++ start-builder.sh
@@ -3,8 +3,11 @@ ARCH=amd64
 PORTAGE_DIR=/home/mzhao/gentoo/portage
 CONFIG_DIR=/home/mzhao/gentoo/gentoo-builder/portage-config-amd64-kde-desktop
 OVERLAY_DIR=/home/mzhao/gentoo/gentoo-builder/overlay-base
+# Output directory
+TARGET_DIR=/home/mzhao/gentoo/output-$ARCH
 docker run --rm -t -i \
   -v ${PORTAGE_DIR}:/usr/portage \
   -v ${CONFIG_DIR}:/etc/portage \
   -v ${OVERLAY_DIR}:/overlay \
+  -v ${TARGET_DIR}:/target \
   -h gentoo-${ARCH}-builder gentoo-${ARCH} /bin/bash
