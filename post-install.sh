83ce04ccdbad40cd18da476515d0bbdd00c6410b
mzhao <mzhao@test8.lab.luminatewireless.com>
2014-11-26 23:15:16 -0800

    set up timezone and locale.

--- post-install.sh
+++ post-install.sh
@@ -5,3 +5,14 @@
 # Update nsscache
 nsscache update --full
 nsscache verify
+
+# Setup timezone
+echo "US/Pacific" > /etc/timezone
+
+# Generate locale
+cat <<EOF > /etc/locale.gen
+en_US ISO-8859-1
+en_US.UTF-8 UTF-8
+EOF
+
+locale-gen
