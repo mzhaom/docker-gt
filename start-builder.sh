b1dcf489a97bcbc28f4941d39057ecea91e8ee5c
mzhao <mzhao@test8.lab.luminatewireless.com>
2014-12-05 14:14:59 -0800

    Allow specify base image name.

--- start-builder.sh
+++ start-builder.sh
@@ -3,6 +3,7 @@ ARCH=amd64
 PORTAGE_DIR=$PWD/portage
 CONFIG_DIR=$PWD/etc-portage
 OVERLAY_DIR=$PWD/overlay
+IMG=${DOCKER_IMG:-gentoo-mini-cooper}
 # Output directory
 # TARGET_DIR=/home/mzhao/gentoo/output-$ARCH
 set -x
@@ -11,4 +12,4 @@ docker run --rm -t -i \
   -v $PWD/packages:/usr/portage/packages \
   -v ${CONFIG_DIR}:/etc/portage \
   -v ${OVERLAY_DIR}:/tmp/overlay \
-  -h $(basename $PWD) gentoo-mini-cooper /bin/bash
+  -h $(basename $PWD) ${IMG} /bin/bash
